ea5ebac63b699415656511de9bbceb28756f2678
root <emploi.neveu@gmail.com>
2018-11-23 13:50:04 +0100

    exo8

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre' );?>
+    <?php
+    foreach ($months as $element) {
+    echo $element  ;
+    }
+    ?>
+  </body>
+</html>
